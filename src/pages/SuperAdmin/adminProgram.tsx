9e6bc3c226f3a8451d1802bcf5f5b5c69af4d139
Abubakar khalid <uog.abubakar@gmail.com>
2024-03-29 10:36:28 +0500

    super admin screen

---
+++ src/pages/SuperAdmin/adminProgram.tsx
@@ -0,0 +1,20 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import { styled } from "@mui/material/styles";
+const StyledBox = styled(Box)(() => ({
+  "& .dashboardCards": {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "0 -12px",
+  },
+}));
+const AdminProgramScreen = () => {
+  return (
+    <StyledBox className="appContainer">
+      <Typography>programs here waiting fromm client</Typography>
+    </StyledBox>
+  );
+};
+
+export default AdminProgramScreen;
---
+++ src/pages/SuperAdmin/departmentDetail.tsx
@@ -0,0 +1,243 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import BackButton from "../../components/Button/backButton";
+import SubHeader from "../../components/SubHeader";
+import TabsArea from "../../components/Tabs";
+
+const StyledBox = styled(Box)(() => ({
+  "& .budgetTitle": {
+    margin: "0 0 10px",
+
+    "& .MuiTypography-body1": {
+      fontSize: "20px",
+      fontWeight: "400",
+      lineHeight: "1.23",
+      letterSpacing: "0.25px",
+    },
+  },
+
+  "& .reviewSubhead": {
+    marginBottom: "4px",
+
+    "& .MuiTypography-h4": {
+      marginTop: "-5px",
+    },
+  },
+
+  "& .backButtonBox": {
+    margin: "-3px 0 8px",
+    paddingLeft: "8px",
+  },
+
+  "& .headerDropdownButton": {
+    marginTop: "6px",
+  },
+
+  "& .totalBudgetText": {
+    fontSize: "18px",
+    fontWeight: "400",
+    marginTop: "-10px",
+    marginBottom: "0",
+  },
+  // Color: theme.palette.secondary.light,
+}));
+const DepartmentDetailScreen = () => {
+  const tableColumnsTitleArray = [
+    [
+      {
+        field: "departmentName",
+        headerName: "Department Name",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "lYearBudget",
+        headerName: "Last Year Budget",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "budget",
+        headerName: "Budget",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "profit",
+        headerName: "Profit",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "nPrograms",
+        headerName: "No. Programs",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "sDate",
+        headerName: "Submission Date",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "comments",
+        headerName: "Comments",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+    ],
+    [
+      {
+        field: "departmentName",
+        headerName: "Department Name",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "lYearBudget",
+        headerName: "Last Year Budget",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "budget",
+        headerName: "Budget",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "profit",
+        headerName: "Profit",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "nPrograms",
+        headerName: "No. Programs",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "sDate",
+        headerName: "Submission Date",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "comments",
+        headerName: "Comments",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+    ],
+    [
+      {
+        field: "departmentName",
+        headerName: "Department Name",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "lYearBudget",
+        headerName: "Last Year Budget",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "budget",
+        headerName: "Budget",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "profit",
+        headerName: "Profit",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "nPrograms",
+        headerName: "No. Programs",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "sDate",
+        headerName: "Submission Date",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+      {
+        field: "comments",
+        headerName: "Comments",
+        sortable: false,
+        editable: false,
+        flex: 1,
+      },
+    ],
+  ];
+  return (
+    <StyledBox className="appContainer">
+      <Box className="backButtonBox">
+        <BackButton />
+      </Box>
+      <SubHeader title="Recreation & Culture" />
+      <Typography className="totalBudgetText">Total Budget: $00,000.00</Typography>
+      <TabsArea
+        tabsTitleArray={[
+          { title: "Pending" },
+          { title: "Rejected" },
+          { title: "Approved" },
+          { title: "Drafts" },
+          { title: "History" },
+        ]}
+        table={tableColumnsTitleArray}
+      />
+    </StyledBox>
+  );
+};
+
+export default DepartmentDetailScreen;
