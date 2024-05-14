68f9e5be69f1c2d6d5661551537f5b9dea118ffc
dev-minified <frontendrock@gmail.com>
2024-05-14 17:27:21 +0500

    changes

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -11,6 +11,6 @@ const ngRockConfig = {
   Base_URL: "https://8281-2401-ba80-aa0f-ef47-f178-3060-59c7-bfdd.ngrok-free.app/api",
 };
 
-const defaultConfig = mainnetConfig;
+const defaultConfig = testnetConfig;
 
 export default defaultConfig;
