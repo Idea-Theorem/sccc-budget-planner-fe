f3f6edbf1e2067e0247372382ead9b31173b5baf
dev-minified <frontendrock@gmail.com>
2024-05-22 16:44:39 +0500

    add search functionality

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,13 @@
-const testnetConfig = {
-  Base_URL: "http://localhost:5000/api",
-
-};
-// const mainnetConfig = {
-//   Base_URL: "http://20.151.79.66/api",
+// const testnetConfig = {
+//   Base_URL: "http://localhost:5000/api",
 
 // };
+const mainnetConfig = {
+  Base_URL: "http://20.151.79.66/api",
+
+};
 
 
-const defaultConfig = testnetConfig;
+const defaultConfig = mainnetConfig;
 
 export default defaultConfig;
