a1073feec8a5158e70b78a2ba4f265df2c12322f
dev-minified <frontendrock@gmail.com>
2024-07-26 16:55:06 +0500

    new changesand fixes

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -25,7 +25,7 @@ const StyledBox = styled(Box)(({ theme }) => ({
   ".MuiDataGrid-virtualScroller": {
     overflowY: "auto",
     overflowX: "hidden",
-  }
+  },
 }));
 const StyleDataGrid = styled(DataGrid)(() => ({
   width: "100%",
@@ -133,8 +133,8 @@ interface ColumnnsProps {
   handleActionReieve?: any;
   loading: boolean;
   currentTab?: any;
-  checkout?: boolean | any
-  handleProgramSearch?: any
+  checkout?: boolean | any;
+  handleProgramSearch?: any;
   approveTabAcriveClass?: boolean | any;
 }
 const TableComponent = (props: ColumnnsProps) => {
@@ -212,19 +212,28 @@ const TableComponent = (props: ColumnnsProps) => {
             </g>
           </g>
         </svg>
-        <Box sx={{ mt: 1 }}>No Rows</Box>
+        {/* <Box>No Rows</Box> */}
       </StyledGridOverlay>
     );
   }
   return (
     <>
-      <StyledBox className={`mainTableBlock ${props.currentTab == "APPROVED" && props?.approveTabAcriveClass ? "approveActiveTab" : "" }`}>
-        <InputSearch placeholder="Search..." onChange={(e: any) => props?.handleProgramSearch(e?.target?.value)} />
+      <StyledBox
+        className={`mainTableBlock ${
+          props.currentTab == "APPROVED" && props?.approveTabAcriveClass
+            ? "approveActiveTab"
+            : ""
+        }`}
+      >
+        <InputSearch
+          placeholder="Search..."
+          onChange={(e: any) => props?.handleProgramSearch(e?.target?.value)}
+        />
         {/* {props.loading && 
         
         <LinearProgress color="success" />
         } */}
-        <Box style={{ height: props.loading ? 300 : "", minHeight: 300  }}>
+        <Box style={{ height: props.loading ? 300 : "", minHeight: 300 }}>
           <StyleDataGrid
             rows={props.loading ? [] : props.row}
             columns={props?.columns}
